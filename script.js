feda500e1463be8698087e65e69049b1cc754245
Natanael Borge <mugiwaranonatha@gmail.com>
2023-02-08 03:22:31 -0400

    Lista de productos dinamica con JS

---
+++ script.js
@@ -0,0 +1,114 @@
+const navBar = document.querySelector('.navbar-email');
+const desktopMenu = document.querySelector('.desktop-menu');
+
+navBar.addEventListener('click', toggleDesktopMenu);
+
+function toggleDesktopMenu(){
+    const isShoppingCarClosed = productDetail.classList.contains('inactive');
+
+    if (!isShoppingCarClosed) {
+        productDetail.classList.add('inactive');
+    }
+
+    desktopMenu.classList.toggle('inactive');
+}
+
+
+
+const burgerMenu = document.querySelector('.menu');
+const toggleBurgerMenu = document.querySelector('.mobile-menu');
+
+burgerMenu.addEventListener('click', toggleMobileMenu);
+
+function toggleMobileMenu() {
+    const isShoppingCarClosed = productDetail.classList.contains('inactive');
+
+    if (!isShoppingCarClosed) {
+        productDetail.classList.add('inactive');
+    }
+
+
+
+    toggleBurgerMenu.classList.toggle('inactive');
+}
+
+
+
+const shoppingCar = document.querySelector('.navbar-shopping-cart');
+const productDetail = document.querySelector('.product-detail');
+
+shoppingCar.addEventListener('click', toggleProductDetail);
+
+function toggleProductDetail () {
+    const isMobileMenuClosed = toggleBurgerMenu.classList.contains('inactive');
+    const isDesktopMenuClosed = desktopMenu.classList.contains('inactive');
+
+    if (!isMobileMenuClosed) {
+        toggleBurgerMenu.classList.add('inactive');
+    }
+
+    if (!isDesktopMenuClosed) {
+        desktopMenu.classList.add('inactive');
+    }
+
+    productDetail.classList.toggle('inactive');
+}
+
+
+const productList = [];
+productList.push({
+    name: 'Bike',
+    price: 120,
+    img: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+productList.push({
+    name: 'Computadora',
+    price: 700,
+    img: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+productList.push({
+    name: 'Playstation 5',
+    price: 650,
+    img: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+productList.push({
+    name: 'Xbox Series S',
+    price: 300,
+    img: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+})
+
+const cardsContainer = document.querySelector('.cards-container');
+
+for (product of productList) {
+    const productCard = document.createElement('div');
+    productCard.classList.add('product-card');
+
+    const productImage = document.createElement('img');
+    productImage.setAttribute('src', product.img);
+
+    const productInfo = document.createElement('div');
+    productInfo.classList.add('product-info');
+
+    const productInfoDiv = document.createElement('div');
+    
+    const productPrice = document.createElement('p')
+    productPrice.innerText = '$' + product.price;
+
+    const productName = document.createElement('p')
+    productName.innerText = product.name;
+
+    productInfoDiv.append(productPrice, productName);
+
+    const productInfoFigure = document.createElement('figure');
+
+    const productImgCard = document.createElement('img');
+    productImgCard.setAttribute('src', './icons/bt_add_to_cart.svg');
+
+    productInfoFigure.appendChild(productImgCard);
+
+    productInfo.append(productInfoDiv, productInfoFigure);
+
+    productCard.append(productImage, productInfo);
+
+    cardsContainer.appendChild(productCard);
+}
